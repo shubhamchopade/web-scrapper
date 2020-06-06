66f2114ba56adfbe081bee33d9cc2dfd60f11cf2
shubhamchopade <31898834+shubhamchopade@users.noreply.github.com>
2020-06-06 11:42:52 +0530

    Add files via upload

---
+++ crawler.py
@@ -0,0 +1,22 @@
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+
+def get_soup_from_url(url: str) -> BeautifulSoup:
+    with urlopen(url) as reply:
+        return BeautifulSoup(reply.read())
+
+
+if __name__ == "__main__":
+    soup = get_soup_from_url("https://www.pixar.com/feature-films/cars-3")
+    print(soup)
+    print(f"\n<{'*' * 104}>\n")
+    links = soup.find_all("img")
+    print(f"\n<{'*' * 104}>\n")
+    print(links)
+    print(f"\n<{'*' * 104}>\n")
+    sources = [src for img in soup.find_all("img") if (src := img.get("src"))]
+    f"{len(sources) / len(links) * 100:.2f}% of imgs have a src attribute."
+    for i, src in enumerate(sources, 1):
+        print(f"{i:>4}: {src}")
